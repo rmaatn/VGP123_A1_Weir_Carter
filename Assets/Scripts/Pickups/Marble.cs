b0af244fa131b077a2c6c46ca04ddb6d073243b2
rmaatn <carter.weir@gmail.com>
2026-09-20 07:44:48 -0400

    Lab 6
    I forgot to make some saves in the previous commit.

--- Assets/Scripts/Pickups/Marble.cs
+++ Assets/Scripts/Pickups/Marble.cs
@@ -13,7 +13,7 @@ public class Marble : MonoBehaviour
 
         int numberOfStyles = anim.runtimeAnimatorController.animationClips.Length;
 
-        if (Style <= 0 || Style > numberOfStyles) Style = Random.Range(1, numberOfStyles + 1);
+        if (Style <= 0 || Style > numberOfStyles) Style = Random.Range(1, numberOfStyles);
 
         anim.Play(Style.ToString());
     }
